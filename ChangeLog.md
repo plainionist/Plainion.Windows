3db2683c195f702eb02ca33a6b3e7bb1df016b39
plainionist <stanley751@gmx.net>
2017-08-06 15:11:32 +0200

    changelog updated

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,6 +1,9 @@
 ## 1.23.0 - unreleased
 
 - Mvvm.DelegateCommand, Mvvm.BindableBase added to avoid referencing Prism library for small projects
+- PropertyBinding.Unbind added
+- Search functionality added to RichTextEditor & NotePad
+- First version of NoteBook added
 
 ## 1.22.0 - 2016-09-06
 
--- src/Plainion.Windows/Controls/Text/FileSystemDocumentStore.cs
+++ src/Plainion.Windows/Controls/Text/FileSystemDocumentStore.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using Plainion.IO;
+using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
-using Plainion.IO;
 
 namespace Plainion.Windows.Controls.Text
 {
