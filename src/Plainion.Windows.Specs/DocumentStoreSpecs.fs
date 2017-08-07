2a266141c56f304e242caa8901c1ccee7051ee15
plainionist <stanley751@gmx.net>
2017-08-07 15:22:55 +0200

    DocumentStore specs completed

--- src/Plainion.Windows.Specs/DocumentStoreSpecs.fs
+++ src/Plainion.Windows.Specs/DocumentStoreSpecs.fs
@@ -5,12 +5,14 @@ open NUnit.Framework
 open Plainion.Windows.Controls.Text
 open Plainion.Windows.Specs.Controls.Text
 open System.IO
+open System.Linq
 open Plainion.IO.MemoryFS
 open FsUnit
 
 [<Apartment(ApartmentState.STA)>]
 [<Spec>]
 module ``Given a DocumentStore`` =
+    open System
 
     let create (fs:FileSystemImpl) = 
         let store = new FileSystemDocumentStore(fs.Directory("/x"))
@@ -61,6 +63,25 @@ module ``Given a DocumentStore`` =
 
         store.Get("/doc1").Body |> text |> should haveSubstring "MORE"
         store.Get("/sub/doc2").Body |> text |> should haveSubstring "MUCH-MORE"
+        
+    [<Test>]
+    let ``<When> removing documents <And> triggering save <Then> the removal is made persistent``() =
+        let fs = new FileSystemImpl()
+        let store = create fs
+
+        store.Create("/doc1").Body |> addText "test1"
+        store.Create("/sub/doc2").Body |> addText "test2"
+
+        store.SaveChanges()
+        let store = create fs
+
+        store.Root.Entries.Remove(store.Get("/doc1")) |> ignore
+        store.Root.IsModified |> should be True
+        
+        store.SaveChanges()
+        let store = create fs
+
+        store.Root.Entries.OfType<Document>() |> List.ofSeq |> should haveLength 0
         
 
 
